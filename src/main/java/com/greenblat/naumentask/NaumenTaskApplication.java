99b2b7bbeb4409ba1c6b318ef30f3f72b258b970
Alex Zhuravlev <leftemail17@mail.ru>
2023-04-12 18:39:10 +0300

    Initial Project Add

---
+++ src/main/java/com/greenblat/naumentask/NaumenTaskApplication.java
@@ -0,0 +1,13 @@
+package com.greenblat.naumentask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaumenTaskApplication.class, args);
+    }
+
+}
