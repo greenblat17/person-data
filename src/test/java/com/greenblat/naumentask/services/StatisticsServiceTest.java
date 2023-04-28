e087782c84dad754ea1866d6a3d6ff97c626529c
Alex Zhuravlev <leftemail17@mail.ru>
2023-04-28 15:49:13 +0300

    delete save in StatisticsServiceTest

--- src/test/java/com/greenblat/naumentask/services/StatisticsServiceTest.java
+++ src/test/java/com/greenblat/naumentask/services/StatisticsServiceTest.java
@@ -1,6 +1,5 @@
 package com.greenblat.naumentask.services;
 
-import com.greenblat.naumentask.model.Person;
 import com.greenblat.naumentask.model.dto.ResponseStatisticsDto;
 import com.greenblat.naumentask.repositories.PersonRepository;
 import com.greenblat.naumentask.repositories.StatisticsRepository;
@@ -10,11 +9,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,14 +30,8 @@ class StatisticsServiceTest {
         // Given
         String name = "Alex";
         int maxAge = 100;
-        int defaultAge = 10;
-        Person person1 = getPerson(name, maxAge);
-        Person person2 = getPerson(name, defaultAge);
-        Person personWithAnotherName = getPerson(anyString(), maxAge);
 
         int count = 10;
-
-        personRepository.saveAll(List.of(person1, person2, personWithAnotherName));
 
         doReturn(count).when(statisticsRepository).findCountByPerson_Name(name);
         doReturn(maxAge).when(personRepository).findMaxAgeByName(name);
@@ -56,16 +47,10 @@ class StatisticsServiceTest {
     }
 
     private ResponseStatisticsDto getStatisticsDto(int count, int age) {
-        return ResponseStatisticsDto.builder()
-                .age(age)
-                .count(count)
-                .build();
+        return new ResponseStatisticsDto(
+                count,
+                age
+        );
     }
 
-    private Person getPerson(String name, int age) {
-        return Person.builder()
-                .name(name)
-                .age(age)
-                .build();
-    }
 }
